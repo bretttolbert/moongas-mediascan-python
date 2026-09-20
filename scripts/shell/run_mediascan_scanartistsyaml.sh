d50a6740142cf7073924bec9e73e49a847fd7690
Brett Tolbert <bretttolbert@gmail.com>
2026-09-19 23:19:55 -0500

    wip

--- scripts/shell/run_mediascan_scanartistsyaml.sh
+++ scripts/shell/run_mediascan_scanartistsyaml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-pushd moongas-go-mediascan > /dev/null
+pushd moongas-mediascan-golang > /dev/null
 go run cmd/mediascan-artists-yaml/main.go "$MOONGAS_COLLECTION_ROOTDIR/mediascan-config.yml" "$MOONGAS_COLLECTION_ROOTDIR/mediascan-artists.yml"
 popd > /dev/null
--- scripts/shell/run_mediascan_scanfilesyaml.sh
+++ scripts/shell/run_mediascan_scanfilesyaml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-pushd moongas-go-mediascan > /dev/null
+pushd moongas-mediascan-golang > /dev/null
 go run cmd/mediascan-files-yaml/main.go "$MOONGAS_COLLECTION_ROOTDIR/mediascan-config.yml" "$MOONGAS_COLLECTION_ROOTDIR/mediascan-files.yml"
 popd > /dev/null
